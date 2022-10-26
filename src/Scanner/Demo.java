ec2032d4db7c6a4a8f5620d7a041c7672d810ca1
mmankar <mmankar@LIN51013850.corp.capgemini.com>
2022-10-26 16:18:12 +0530

    First commit...

---
+++ src/Scanner/Demo.java
@@ -0,0 +1,12 @@
+package Scanner;
+
+public class Demo {
+	public static void main(String[] args) {
+		String str1=new String("array element");
+		System.out.println(str1.toUpperCase());
+		String str2=new String("MANGO");
+		System.out.println(str2.toLowerCase());
+		System.out.println(str1.concat( str2));
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mmankar
+ *
+ */
+module Gitdemo {
+}
